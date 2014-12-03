67020e9260bcce66ce58e08d4cc1b3e6f5c4067d
Juan Lebrijo <juan@lebrijo.com>
2014-12-03 11:02:42 +0100

    Step 2. Charging this course as recurring

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,13 +2,14 @@ class Registration < ActiveRecord::Base
   belongs_to :course
 
   def process_payment
-    customer = Stripe::Customer.create email: self.email,
-                                       card: self.card_token
+    customer_data = {email: self.email, card: self.card_token}
+                        .merge( (self.course.plan.blank?)? {}: {plan: self.course.plan})
+    customer = Stripe::Customer.create customer_data
 
     Stripe::Charge.create customer: customer.id,
                           amount: self.course.price*100,
                           description: self.course.name,
                           currency: 'usd'
-
+    return customer.id
   end
 end
