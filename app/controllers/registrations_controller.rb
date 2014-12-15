028928d9911e3ee08686b78289b058b7a64b2aba
Juan Lebrijo <juan@lebrijo.com>
2014-12-15 17:42:00 +0100

    Post 1 fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,10 +20,11 @@ class RegistrationsController < ApplicationController
   def create
     @registration = Registration.new registration_params.merge(email: stripe_params["stripeEmail"],
                                                                card_token: stripe_params["stripeToken"])
+    raise "Please, check registration errors" unless @registration.valid?
     @registration.process_payment
     @registration.save
     redirect_to @registration, notice: 'Registration was successfully created.'
-  rescue Stripe::CardError => e
+  rescue
     flash[:error] = e.message
     render :new
   end
